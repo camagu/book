f834355f6296723db709a66bf79df55a680e901b
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-08-13 22:12:48 -0500

    Separate chapters styles

--- lib/chapter_generator.rb
+++ lib/chapter_generator.rb
@@ -2,7 +2,8 @@ def generate_chapters
   pages.each_pair do |chapter, sections|
     @items << Nanoc::Item.new(
       "==render 'chapter', chapter: '#{chapter}'",
-      { title: "Capitulo #{chapter}" },
+      { title: "Capitulo #{chapter}",
+        stylesheets: ["/style/chapters/#{chapter.sub('.', '_')}.css"] },
       "/capitulo-#{chapter}"
     )
   end
